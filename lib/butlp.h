7c1d108b47bdc04412fe329acb2a7a8b352354c9
Carl Aksynczak <carl.aksynczak@gmail.com>
2020-02-23 16:56:16 -0500

    Filtre Butterworth passe bas - Carl Aks

---
+++ lib/butlp.h
@@ -0,0 +1,56 @@
+#ifndef __BUTLP_H__
+#define __BUTLP_H__
+
+/* Second-order Butterwoth lowpass filter
+ *
+ * 	float freq;					frequency in Hz
+ *	float pi_over_sr;			calcultate pi / sample rate
+ *	float nyquist;				calculate Nyquist frequency
+ *	float sqrt_2;				calculate square root of 2
+ *	float x1, x2, y1, y2;		input and output retained value
+ *	float a0, a1, a2, b1, b2;   coefficient calculation
+ *  
+ */
+struct butlp {
+	float freq;
+	float pi_over_sr;
+	float nyquist;
+	float sqrt_2;
+	float x1, x2, y1, y2;
+	float a0, a1, a2, b1, b2;
+};
+
+/* Initialize the Butterwoth lowpass filter's structure.
+ *
+ * freq:    Desired frequency in Hz.
+ * sr:      Sampling rate in Hz.
+ */
+struct butlp * butlp_init(float freq, float sr);
+
+/* Delete Butterwoth lowpass filter's structure.
+ *
+ * struct butlp *data: The structure for which to release memory.
+ */
+void butlp_delete(struct butlp *data);
+
+/* Process the next sample of Butterwoth lowpass filter's.
+ *
+ * struct butlp *data: The structure for which to compute the next sample.
+ */
+float butlp_process(struct butlp *data, float input);
+
+/* Change the frequency of Butterwoth lowpass filter's.
+ *
+ * struct butlp *data: The structure for which to set a new frequency.
+ * float input:        Input signal
+ */
+
+/* Calculate de coefficient of Butterwoth lowpass filter's 
+ * on with a new frequency.
+ * 
+ * struct butlp *data: to uptade all coefficients
+ * float freq :		set new frequency
+ */
+void butlp_compute_coeffs(struct butlp *data, float freq);
+
+#endif
---
+++ main_CA_butlp.c
@@ -0,0 +1,190 @@
+/*
+ * Template file to create a live audio processing program with portaudio.
+ *
+ * Compile on linux and MacOS with:
+ *  gcc main_CA_butlp.c lib/*.c -Ilib -lm -lportaudio -o main_CA_butlp
+ *
+ * Compile on Windows with:
+ *  gcc main_CA_butlp.c lib/*.c -Ilib -lm -lportaudio -o main_CA_butlp.exe
+ *
+ * Run on linux and MacOS with:
+ *  ./main_CA_butlp
+ *
+ * Run on Windows with:
+ *  main_CA_butlp.exe
+*/
+
+/* System includes. */
+#include <stdlib.h>     /* malloc, free */
+#include <stdio.h>      /* printf, fprintf, getchar, stderr */    
+
+//== Program-specific system includes. ==
+// This is where you include the program-specific system headers (if needed) by your program...
+
+/* Include all portaudio functions. */
+#include "portaudio.h"
+
+/* Define global audio parameters, used to setup portaudio. */
+#define SAMPLE_RATE         44100
+#define FRAMES_PER_BUFFER   512
+#define NUMBER_OF_CHANNELS  2
+
+
+//== Program-specific includes. ==
+// This is where you include the specific headers needed by your program...
+#include "butlp.h"
+
+//== Program-specific parameters. ==
+// This is where you define the specific parameters needed by your program...
+#define FREQ           800
+
+/* The DSP structure contains all needed audio processing "objects". */
+struct DSP {
+    // This is where you declare the specific processing structures
+    // needed by your program... Each declaration should have the form:
+
+    // struct delay *delayline[NUMBER_OF_CHANNELS];
+    struct butlp *filter[NUMBER_OF_CHANNELS];
+
+    // Which means a "multi-channel" pointer to the processing structure.
+
+};
+
+/* This function allocates memory and intializes all dsp structures. */
+struct DSP * dsp_init() {
+    int i;
+    struct DSP *dsp = malloc(sizeof(struct DSP));   /* Memory allocation for DSP structure. */
+    for (i = 0; i < NUMBER_OF_CHANNELS; i++) {
+        // This is where you setup the specific processing structures needed by your program,
+        // using the provided xxx_init functions. Something like:
+
+        // dsp->delayline[i] = delay_init(DELTIME, SAMPLE_RATE);
+        dsp->filter[i] = butlp_init(FREQ, SAMPLE_RATE);
+
+    }
+    return dsp;
+}
+
+/* This function releases memory used by all dsp structures. */
+void dsp_delete(struct DSP *dsp) {
+    int i;
+    for (i = 0; i < NUMBER_OF_CHANNELS; i++) {
+        // This is where you release the memory used by the specific processing structure
+        // used in the program. Something like:
+
+        // delay_delete(dsp->delayline[i]);
+        butlp_delete(dsp->filter[i]);
+
+    }
+    free(dsp);
+}
+
+/* This function does the actual processing chain. */
+void dsp_process(const float *in, float *out, unsigned long framesPerBuffer, struct DSP *dsp) {
+    unsigned int i, j, index;   /* Variables used to compute the index of samples in input/output arrays. */
+
+    // Add any variables useful to your processing logic here...
+
+    for (i=0; i<framesPerBuffer; i++) {             /* For each sample frame in a buffer size... */
+        for (j=0; j<NUMBER_OF_CHANNELS; j++) {      /* For each channel in a frame... */
+            index = i * NUMBER_OF_CHANNELS + j;     /* Compute the index of the sample in the arrays... */
+
+            // This is where you want to put your processing logic... A simple thru is:
+            // out[index] = in[index];
+            out[index] = butlp_process(dsp->filter[j],in[index]);
+        }
+    }
+}
+
+/**********************************************************************************************
+ *
+ * You shouldn't need to edit anything below here !
+ *
+ *********************************************************************************************/
+
+static int callback(const void *inputBuffer, void *outputBuffer,
+                    unsigned long framesPerBuffer,
+                    const PaStreamCallbackTimeInfo* timeInfo,
+                    PaStreamCallbackFlags statusFlags, void *userData) 
+{
+    const float *in = (const float *)inputBuffer;
+    float *out = (float *)outputBuffer;
+    /* Prevent unused variable warnings. */
+    (void) timeInfo;
+    (void) statusFlags;
+
+    struct DSP *dsp = (struct DSP *) userData;
+
+    if (inputBuffer == NULL) { return paAbort; }
+
+    dsp_process(in, out, framesPerBuffer, dsp);
+    
+    return paContinue;
+}
+
+int paErrorCheck(PaError err)
+{
+    if (err != paNoError) {
+        Pa_Terminate();
+        fprintf(stderr,  Pa_GetErrorText(err));
+        return 1;
+    }
+    return 0;
+}
+
+int paDefaultDeviceCheck(PaDeviceIndex device, char *direction)
+{
+    if (device == paNoDevice) {
+        Pa_Terminate();
+        fprintf(stderr, "Error: No default %s device.\n", direction);
+        return 1;
+    }
+    return 0;
+}
+
+int main(void)
+{
+    PaStreamParameters inputParameters, outputParameters;
+    PaStream *stream;
+    PaError err;
+
+    struct DSP *dsp = dsp_init();
+    
+    err = Pa_Initialize();
+    if (paErrorCheck(err)) { return -1; }
+
+    inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
+    if (paDefaultDeviceCheck(inputParameters.device, "input")) { return -1; }
+
+    inputParameters.channelCount = NUMBER_OF_CHANNELS;
+    inputParameters.sampleFormat = paFloat32;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
+    inputParameters.hostApiSpecificStreamInfo = NULL;
+
+    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+    if (paDefaultDeviceCheck(outputParameters.device, "output")) { return -1; }
+
+    outputParameters.channelCount = NUMBER_OF_CHANNELS;
+    outputParameters.sampleFormat = paFloat32;
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+
+    err = Pa_OpenStream(&stream, &inputParameters, &outputParameters, SAMPLE_RATE,
+                        FRAMES_PER_BUFFER, paNoFlag, callback, (void *)dsp);
+    if (paErrorCheck(err)) { return -1; }
+
+    err = Pa_StartStream(stream);
+    if (paErrorCheck(err)) { return -1; }
+
+    printf("Hit ENTER to stop program.\n");
+    getchar();
+
+    err = Pa_CloseStream(stream);
+    if (paErrorCheck(err)) { return -1; }
+
+    dsp_delete(dsp);
+
+    Pa_Terminate();
+
+    return 0;
+}
