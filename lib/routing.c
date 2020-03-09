8349359cc1274f2cd7e866ffa148b45500408c2c
fabien_lamarche-filion <contested.space@gmail.com>
2020-03-08 23:13:09 -0400

    Add input/bus verification and error handling

--- lib/routing.c
+++ lib/routing.c
@@ -2,25 +2,43 @@
    Fabien Lamarche-Filion, 02 March 2020
 */
 
-#include<stdlib.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "routing.h"
 #include "utils.h"
 
 struct routing_matrix * routing_matrix_init() {
   struct routing_matrix * matrix = calloc(1, sizeof(struct routing_matrix));
-  for (int i = 0; i < MAX_BUSSES; i++){
+  for (size_t i = 0; i < MAX_BUSSES; i++){
     matrix->bus[i].state = OFF; 
   }
   return matrix;
 }
 
 void matrix_route(struct routing_matrix * mat, int input, int bus) {
+  if (input < 0 || input >= MAX_INPUTS ||
+      bus < 0 || bus >= MAX_BUSSES){
+    printf("[ROUTING MATRIX WARNING] Invalid matrix_route:\n\tinput: %d -> bus: %d\n",
+	   input, bus);
+    return;
+  }
+  
   mat->bus[bus].input = input;
   mat->bus[bus].state = ON;
   mat->bus[bus].type = SINGLE;
 }
 
 void matrix_route_mix(struct routing_matrix * mat, int input, int input_alt, float mix, int bus) {
+  if (input < 0 || input >= MAX_INPUTS ||
+      input_alt < 0 || input_alt >= MAX_INPUTS ||
+      bus < 0 || bus >= MAX_BUSSES){   
+    printf("[ROUTING MATRIX WARNING] Invalid matrix_route_mix:\n\tinput: %d + input_alt: %d -> bus: %d\n",
+	   input, input_alt, bus);
+
+    return;
+  }
+
   mat->bus[bus].input = input;
   mat->bus[bus].input_alt = input_alt;
   mat->bus[bus].state = ON;
@@ -29,6 +47,11 @@ void matrix_route_mix(struct routing_matrix * mat, int input, int input_alt, flo
 }
 
 float matrix_bus_output(struct routing_matrix * mat, int bus, float default_value) {
+  if (bus < 0 || bus >= MAX_BUSSES) {
+    printf("[ROUTING MATRIX WARNING] Invalid matrix_bus_output:\n\tbus: %d\n", bus);
+    return default_value;
+  }
+
   if (mat->bus[bus].state == ON) {
     return mat->bus[bus].output;
   }
@@ -36,10 +59,20 @@ float matrix_bus_output(struct routing_matrix * mat, int bus, float default_valu
 }
 
 void matrix_set_bus_mix(struct routing_matrix * mat, int bus, float mix){
+  if (bus < 0 || bus >= MAX_BUSSES) {    
+    printf("[ROUTING MATRIX WARNING] Invalid matrix_set_bus_mix:\n\tbus: %d\n", bus);
+    return;
+  }
+  
   mat->bus[bus].mix = mix;
 }
 
 void matrix_kill_bus(struct routing_matrix * mat, int bus){
+  if (bus < 0 || bus >= MAX_BUSSES) {    
+    printf("[ROUTING MATRIX WARNING] Invalid matrix_kill_bus:\n\tbus: %d\n", bus);
+    return;
+  }
+
   mat->bus[bus].state = OFF;
 }
 
@@ -48,6 +81,11 @@ void delete_routing_matrix(struct routing_matrix * mat) {
 }
 
 void matrix_update_input(struct routing_matrix * mat, int input, float value) {
+  if (input < 0 || input >= MAX_INPUTS) {    
+    printf("[ROUTING MATRIX WARNING] Invalid matrix_update_input:\n\tinput: %d\n", input);
+    return;
+  }
+  
   mat->inputs[input] = value;
 }
 
