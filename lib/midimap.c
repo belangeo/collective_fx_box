8f0f5a82871ea62104ed8179cfa1c5f1b7555586
Olivier Belanger <belangeo@gmail.com>
2020-03-30 00:32:17 -0400

    Try to first open the midi config from the current working directory, else look into config folder.

--- lib/midimap.c
+++ lib/midimap.c
@@ -8,13 +8,18 @@ static char midimap_str[128][128];
 void midimap_init() {
     int i, ctlnum;
     char name[128];
+    FILE *config;
 
     for (i=0; i<128; i++) {
         snprintf(name, 128, "%i", i);
         strcpy(midimap_str[i], name);
     }
 
-    FILE *config = fopen("config/midimap.conf", "r");
+    config = fopen("midimap.conf", "r");
+    if (config == NULL) {
+        config = fopen("config/midimap.conf", "r");
+    }
+
     if (config != NULL) {
         while (fscanf(config, "%s %i\n", name, &ctlnum) != EOF) {
             strcpy(midimap_str[ctlnum], name);
